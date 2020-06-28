83557281d68095707b2da07a1bd1655b9df1af85
gitysubh <pal.subhashis62@gmail.com>
2020-06-28 17:11:21 +0530

    - Basic use of Text, Image, Button Component - Use Platform and StatusBar Api

--- App.js
+++ App.js
@@ -1,21 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  SafeAreaView,
+  Button,
+  Alert,
+  Platform,
+  StatusBar,
+} from "react-native";
 
 export default function App() {
+  const [isImageShown, setImamgeDisplayState] = useState(true);
+
+  const toggleImage = () => {
+    const message = isImageShown
+      ? "Are you sure to hide the image"
+      : "Are you sure to show the image";
+
+    Alert.alert("Confirm", message, [
+      {
+        text: "Yes",
+        onPress: () => setImamgeDisplayState(!isImageShown),
+        style: { backgroundColor: "Green" },
+      },
+      {
+        text: "No",
+      },
+    ]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Hello fucking world. How are you</Text>
+      <Text>
+        I love India. Making this text such a long one. Like to see the line
+        break case
+      </Text>
+      {isImageShown && (
+        <Image
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      )}
+      <Button
+        onPress={toggleImage}
+        title={isImageShown ? "Hide Image" : "Show Image"}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    padding: 3,
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
